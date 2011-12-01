b2e2ce34d1efeba120bdb17f8c759d234e51445e
Whit Morriss <whit@surveymonkey.com>
2011-11-30 20:59:07 -0600

    use path.py, add whitespace

--- cheeseprism/pipext.py
+++ cheeseprism/pipext.py
@@ -3,15 +3,17 @@ from pip.locations import build_prefix, src_prefix
 from pip.log import logger
 from pip.req import RequirementSet, parse_requirements
 from pip.util import display_path
-import os
-import shutil
+from path import path
+
 
 def parse_reqs(filename, download_dir):
-    build_dir = os.path.abspath(build_prefix)
-    src_dir = os.path.abspath(src_prefix)
+    build_dir = path(build_prefix).abspath()
+    src_dir = path(src_prefix).abspath()
+
     finder = PackageFinder(
         find_links=[],
         index_urls=['http://pypi.python.org/simple'])
+
     requirement_set = CheeseRequirementSet(
         build_dir=build_dir,
         src_dir=src_dir,
@@ -20,26 +22,30 @@ def parse_reqs(filename, download_dir):
         upgrade=False,
         ignore_installed=True,
         ignore_dependencies=False)
+
     Options = type('Options', (),{})
     Options.skip_requirements_regex = ''
     Options.default_vcs = ''
     options = Options()
     names = []
+
     for req in parse_requirements(filename, finder=finder, options=options):
         requirement_set.add_requirement(req)
         names.append(req.req)
+
     requirement_set.prepare_files(finder, force_root_egg_info=False, bundle=False)
     return names
+
 
 class CheeseRequirementSet(RequirementSet):
     def copy_file(self, filename, location, content_type, link):
         copy = True
-        download_location = os.path.join(location, link.filename)
-        if os.path.exists(download_location):
+        download_location = path(location) / link.filename
+        if download_location.exists():
             copy = False
             logger.notify('Ignoring already existing file.')
         if copy:
-            shutil.copy(filename, download_location)
+            download_location.copy(filename)
             logger.indent -= 2
             logger.notify('Saved %s' % display_path(download_location))
     
