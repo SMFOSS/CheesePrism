11da9eba3ead2312c80de9dbe353921f2e8dff8e
Whit Morriss <whit@surveymonkey.com>
2011-12-07 15:06:32 -0600

    Be explicit about dependency on Jinja2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-requires=['pyramid', 'pyyaml', 'pyramid_jinja2', 'path.py', 'werkzeug', 'pyramid_debugtoolbar']
+requires=['pyramid', 'pyyaml', 'Jinja2', 'pyramid_jinja2', 'path.py', 'werkzeug', 'pyramid_debugtoolbar']
 
 setup(name='CheesePrism',
       version='0.0',
