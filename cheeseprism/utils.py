7e88f08b39095e015ee21aca335881e1d84dac2c
Whit Morriss <whit@surveymonkey.com>
2011-12-07 15:03:52 -0600

    remove away

--- cheeseprism/utils.py
+++ cheeseprism/utils.py
@@ -1,170 +1,12 @@
-from path import path
 from werkzeug.contrib.cache import SimpleCache
-import os
-import shutil
-import subprocess
-import sys
-import tarfile
-import tempfile
-import xmlrpclib
-import zipfile
 import logging
+import pkg_resources
+import xmlrpclib
+
 
 logger = logging.getLogger(__name__)
 
-
-class TarArchive:
-    def __init__(self, filename):
-        self.filename = filename
-	mode = "r"
-	if filename.endswith('.bz2'):
-	    mode = "r:bz2"
-	elif filename.endswith('.gz') or filename.endswith('.tgz'):
-	    mode = "r:gz"
-        self.tgz = tarfile.TarFile.open(filename, mode)
-
-    def names(self):
-        return self.tgz.getnames()
-
-    def lines(self, name):
-        return self.tgz.extractfile(name).readlines()
-
-    def extract(self, name, tempdir):
-        return self.tgz.extract(name, tempdir)
-
-    def extractall(self, tempdir):
-        os.system('cd %s && tar xzf %s' % (tempdir, 
-                                           os.path.abspath(self.filename)))
-
-    def close(self):
-        return self.tgz.close()
-
- 
-class ZipArchive:
-    def __init__(self, filename):
-        self.filename = filename
-        self.zipf = zipfile.ZipFile(filename, 'r')
-
-    def names(self):
-        return self.zipf.namelist()
-
-    def lines(self, name):
-        return self.zipf.read(name).split('\n')
-
-    def extract(self, name, tempdir):
-        data = self.zipf.read(name)
-        fn = name.split(os.sep)[-1]
-        fn = os.path.join(tempdir, fn)
-        f = open(fn, 'wb')
-        f.write(data)
-
-    def extractall(self, tempdir):
-        os.system('cd %s && unzip %s' % (tempdir, 
-                                         os.path.abspath(self.filename)))
-
-    def close(self):
-        return self.zipf.close()
-
-def _extract_name_version(filename, tempdir):
-    archive = None
-    if filename.endswith('.gz') or filename.endswith('.tgz') or filename.endswith('.bz2'):
-        archive = TarArchive(filename)
-
-    elif filename.endswith('.egg') or filename.endswith('.zip'):
-        archive = ZipArchive(filename)
-    
-    if archive is None:
-        return
-    try:
-        for name in archive.names():
-            if len(name.split('/'))==2  and name.endswith('PKG-INFO'):
-                project, version = None, None
-                lines = archive.lines(name)
-                for line in lines:
-                    key, value = line.split(':', 1)
-                    if key == 'Name':
-                        print filename, value
-                        project = value.strip()
-                        
-                    elif key == 'Version':
-                        version = value.strip()
-                    
-                    if project is not None and version is not None:
-                        return project, version
-                    continue
-        archive.extractall(tempdir)
-        dirs = os.listdir(tempdir)
-        dir = os.path.join(tempdir, dirs[0])
-        if not os.path.isdir(dir):
-            dir = tempdir
-        command = ('cd %s && %s setup.py --name --version'
-                       % (dir, sys.executable))
-        popen = subprocess.Popen(command,
-                                stdout=subprocess.PIPE,
-                                shell=True,
-                                )
-        output = popen.communicate()[0]
-        archive.close()
-        return output.splitlines()[:2]
-    except:
-        archive.close()
-        import traceback
-        print traceback.format_exc()
-    return
-        
-def regenerate_index(indexpath, filename):
-    #print "Regenerate index"
-    indexpath = path(indexpath)
-    if not indexpath.exists():
-        indexpath.makedirs()
-    projects = {}
-    files = indexpath.files('*.egg|*.gz|*.bz2|*.tgz|*.zip') # make list configurable
-    for item in files:
-        try:
-            tempdir = tempfile.mkdtemp()
-            itempath = indexpath / item
-            logger.debug("Processing %s", itempath)
-            project, revision = _extract_name_version(itempath, tempdir)
-            projects.setdefault(project, []).append((revision, item))
-            path(tempdir).rmtree()
-        except:
-            import traceback
-            tb = traceback.format_exc()
-            logger.error(tb)
-    
-    items = projects.items()
-    items.sort()
-
-    #@@ <needs to be templatized >
-    f = indexpath / filename
-    f.write_lines(['<html>\n',
-                    '<body>\n',
-                    '<h1>Package Index</h1>\n',
-                    '<ul>\n'])
-    
-    for key, value in items:
-        dirname = indexpath / key
-        if not dirname.exists():
-            dirname.makedirs()
-        f.write_text('<li><a href="%s/index.html">%s</a>\n' % (key, key), append=True)
-        sub = indexpath / key / 'index.html' 
-        sub.write_lines(['<html>\n',
-                        '<body>\n',
-                        '<h1>%s Distributions</h1>\n' % key,
-                        '<ul>\n'])
-        for revision, archive in value:
-            print '  -> %s, %s' % (revision, archive)
-            sub.write_text('<li><a href="../%s">%s</a>\n' % (archive.name, archive.name), append=True)
-        
-        sub.write_lines(['</ul>',
-                        '</body>',
-                        '</html>'], append=True)
-    f.write_lines(['</ul>',
-                    '</body>',
-                    '</html>'], append=True)
-    #@@ </needs to be templatized >
-
-
+#@@ refactor into class
 cache = SimpleCache()
 
 def list_pypi():
@@ -188,3 +30,28 @@ def search_pypi(package_name):
 def package_details(package_name, version_number):
     client = xmlrpclib.ServerProxy('http://pypi.python.org/pypi')
     return client.release_urls(package_name,version_number)
+
+
+def resource_spec(spec):
+    """
+    Loads resource from a string specifier. 
+    >>> from doula.utils import resource_spec
+    >>> resource_spec('egg:ReleaseDoula#doula/roles.yml')
+    '.../doula/roles.yml'
+
+    >>> resource_spec('file:data/languages.ini')
+    'data/languages.ini'
+
+    >>> resource_spec('data/languages.ini')
+    'data/languages.ini'
+    """
+    filepath = spec
+    if spec.startswith('egg:'):
+        req, subpath = spec.split('egg:')[1].split('#')
+        req = pkg_resources.get_distribution(req).as_requirement()
+        filepath = pkg_resources.resource_filename(req, subpath)
+    elif spec.startswith('file:'):
+        filepath = spec.split('file:')[1]
+    # Other specs could be added, but egg and file should be fine for
+    # now
+    return filepath
