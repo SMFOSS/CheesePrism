3862eca96d1c7f808b16949bfc45797502ad09a2
Whit Morriss <whit@surveymonkey.com>
2011-12-12 13:40:17 -0600

    templating for index
     * add a loader for the special environment
     * add special package directory

--- cheeseprism/__init__.py
+++ cheeseprism/__init__.py
@@ -27,8 +27,42 @@ def main(global_config, **settings):
     config.scan('cheeseprism.views')
     config.set_request_factory(CPRequest)
     config.add_route('package', 'package/{name}/{version}', view='cheeseprism.views.package')
+    config.index_templates = EnvFactory.from_config(settings['cheeseprism.index_templates'])
 
     return config.make_wsgi_app()    
+
+
+class EnvFactory(object):
+    env_class = jinja2.Environment
+    def __init__(self, config):
+        self.config = config
+
+    @property
+    def loaders(self):
+        if self.config:
+            loaders = self.config.split(' ')
+            for loader in loaders:
+                spec = loader.split(':')
+                if len(spec) == 1:
+                    yield jinja2.FileSystemLoader(spec); continue
+
+                type_, spec = spec
+                if type_ == "file":
+                    yield jinja2.FileSystemLoader(spec); continue
+
+                if type_ == 'pkg':
+                    spec = spec.split('#')
+                    if len(spec) == 1: yield jinja2.PackageLoader(spec[0])
+                    else: yield jinja2.PackageLoader(*spec)
+                    continue
+                raise RuntimeError('Loader type not found: %s %s' %(type_, spec))
+
+    @classmethod
+    def from_config(cls, config=None):
+        factory = cls(config)
+        choices = [jinja2.PackageLoader('cheeseprism', 'templates/index')]
+        if config: [choices.insert(0, loader) for loader in factory.loaders]
+        return factory.env_class(loader=jinja2.ChoiceLoader(choices))
 
 
 class CPRequest(Request):
@@ -39,6 +73,11 @@ class CPRequest(Request):
     @reify
     def settings(self):
         return self.registry.settings
+
+    @reify
+    def index_templates(self):
+        return self.registry.index_templates
+    
 
     @reify
     def file_root(self):
