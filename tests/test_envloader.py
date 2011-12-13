1445ce9a5c05718ab69df500f8296e1d2a112bf9
Whit Morriss <whit@surveymonkey.com>
2011-12-13 15:11:34 -0600

    fix envloader test

--- tests/test_envloader.py
+++ tests/test_envloader.py
@@ -4,13 +4,13 @@ from path import path
 
 def test_envfactory_makes_loaders():
     from cheeseprism import EnvFactory
-    env = EnvFactory.from_config('pkg:cheeseprism pkg:cheeseprism#templates/fake file:/tmp/ %s' %path(__file__).parent)
+    env = EnvFactory.from_str('pkg:cheeseprism pkg:cheeseprism#templates/fake file:/tmp/ %s' %path(__file__).parent)
     assert isinstance(env.loader, jinja2.loaders.ChoiceLoader)
     assert len(env.loader.loaders) == 5
     # is this backwards?
-    assert env.loader.loaders[1].searchpath[0].endswith('tests')
+    assert env.loader.loaders[0].searchpath[0].endswith('tests'),  env.loader.loaders[1].searchpath
     assert env.loader.loaders[1].searchpath == ['/tmp/']
-    assert env.loader.loaders[-1].package_path == 'template/index'
+    assert env.loader.loaders[-1].package_path == 'templates/index', env.loader.loaders[-1].package_path
     assert env.loader.loaders[-2].package_path == 'templates'
     assert env.loader.loaders[-3].package_path == 'templates/fake'
     
