aa695ab796079e5806cf812d222b1456e23a8562
Whit Morriss <whit@surveymonkey.com>
2011-12-29 15:49:59 -0600

    comment out unused traversal helpers

--- cheeseprism/resources.py
+++ cheeseprism/resources.py
@@ -1,32 +1,31 @@
-from pyramid.url import resource_url
-
+#from pyramid.url import resource_url
 
 class BaseResource(dict):
     """
     Base class for resources
     """
 
-    @property
-    def approot(self):
-        return self.recurse_parent(App)
+##     @property
+##     def approot(self):
+##         return self.recurse_parent(App)
             
-    def recurse_parents(self, klass, parent=None):
-        if parent is None:
-            parent = self.__parent__
-        else:
-            parent = parent.__parent__
-        if parent is None:
-            # we've rooted out
-            return None
-        if isinstance(parent, klass):
-            return parent
-        return self.recurse_parent(klass, parent)
+##     def recurse_parents(self, klass, parent=None):
+##         if parent is None:
+##             parent = self.__parent__
+##         else:
+##             parent = parent.__parent__
+##         if parent is None:
+##             # we've rooted out
+##             return None
+##         if isinstance(parent, klass):
+##             return parent
+##         return self.recurse_parent(klass, parent)
     
-    def suburls(self, request):
-        for name in sorted(self):
-            obj = self[name]
-            url = resource_url(self, request, name)
-            yield obj, url
+##     def suburls(self, request):
+##         for name in sorted(self):
+##             obj = self[name]
+##             url = resource_url(self, request, name)
+##             yield obj, url
 
 
 class App(BaseResource):
