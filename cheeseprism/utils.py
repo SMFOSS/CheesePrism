490331e899f1577f25e7165efff63f641071d124
Whit Morriss <whit@surveymonkey.com>
2012-01-01 15:41:40 -0600

    sequester to utils for now
    considering just copying secure_filename and simplecache,

--- cheeseprism/utils.py
+++ cheeseprism/utils.py
@@ -1,4 +1,7 @@
+#import os
+#import re
 from werkzeug.contrib.cache import SimpleCache
+from werkzeug.utils import secure_filename
 import logging
 import pkg_resources
 import xmlrpclib
@@ -55,3 +58,53 @@ def resource_spec(spec):
     # Other specs could be added, but egg and file should be fine for
     # now
     return filepath
+
+
+
+## _windows_device_files = ('CON', 'AUX', 'COM1', 'COM2', 'COM3', 'COM4', 'LPT1',
+##                          'LPT2', 'LPT3', 'PRN', 'NUL')
+## _filename_ascii_strip_re = re.compile(r'[^A-Za-z0-9_.-]')
+
+## def secure_filename(filename):
+##     r"""Pass it a filename and it will return a secure version of it.  This
+##     filename can then safely be stored on a regular file system and passed
+##     to :func:`os.path.join`.  The filename returned is an ASCII only string
+##     for maximum portability.
+
+##     On windows system the function also makes sure that the file is not
+##     named after one of the special device files.
+
+##     >>> secure_filename("My cool movie.mov")
+##     'My_cool_movie.mov'
+##     >>> secure_filename("../../../etc/passwd")
+##     'etc_passwd'
+##     >>> secure_filename(u'i contain cool \xfcml\xe4uts.txt')
+##     'i_contain_cool_umlauts.txt'
+
+##     The function might return an empty filename.  It's your responsibility
+##     to ensure that the filename is unique and that you generate random
+##     filename if the function returned an empty one.
+
+##     .. versionadded:: 0.5
+
+##     :param filename: the filename to secure
+
+##     Credit: from Armin Ronacher's Werkzeug, BSD
+##     """
+##     if isinstance(filename, unicode):
+##         from unicodedata import normalize
+##         filename = normalize('NFKD', filename).encode('ascii', 'ignore')
+##     for sep in os.path.sep, os.path.altsep:
+##         if sep:
+##             filename = filename.replace(sep, ' ')
+##     filename = str(_filename_ascii_strip_re.sub('', '_'.join(
+##                    filename.split()))).strip('._')
+
+##     # on nt a couple of special files are present in each folder.  We
+##     # have to ensure that the target file is not such a filename.  In
+##     # this case we prepend an underline
+##     if os.name == 'nt' and filename and \
+##        filename.split('.')[0].upper() in _windows_device_files:
+##         filename = '_' + filename
+
+##     return filename
